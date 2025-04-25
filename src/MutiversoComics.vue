108171883c3e7f0e1ac91043b4f5151f813fe0fb
Andersonramos2023 <andersonrsanjo@gmail.com>
2025-04-25 06:52:18 -0300

    carinho-

--- src/MutiversoComics.vue
+++ src/MutiversoComics.vue
@@ -1,30 +1,41 @@
 <template>
+
   <head>
     <link rel="stylesheet" href="components/MutiversoComics.css">
 
   </head>
   <body>
       <header>
-        <section  class="autor">
-        <button><a href="http://pt.wikipedia.org/wiki/Stan_Lee#:~:text=Stanley%20Martin%20Lieber%2C%20mais%20conhecido,empres%C3%A1rio%20e%20ator%20norte-americano.">Altor da comic</a></button>
+
+        <div class="cabecalho">
+  <a href="MutiversoComics.vue">Multiverso Comics</a>
+        </div>
+
+        <section class="Altor">
+
+          <div id="tset">
+
+            <a href="http://pt.wikipedia.org/wiki/Stan_Lee#:~:text=Stanley%20Martin%20Lieber%2C%20mais%20conhecido,empres%C3%A1rio%20e%20ator%20norte-americano.">Altor da comic</a>
+
 
         <h1>Stanley Martin Lieber</h1>
 
-        <p>Mais conhecido como Stan Lee (Nova Iorque, 28 de dezembro de 1922 — Los Angeles, 12 de novembro de 2018), foi um escritor, editor, publicitário, produtor, diretor, empresário e ator norte-americano.
+        <p>
+          Mais conhecido como Stan Lee (Nova Iorque, 28 de dezembro de 1922 — Los Angeles, 12 de novembro de 2018), foi um escritor, editor, publicitário, produtor, diretor, empresário e ator norte-americano, Mais conhecido como o criador da Marvel.
         </p>
 
-        <button class="acessarLivro"> <a href="https://pt.scribd.com/document/596897611/Capitao-America-Comics-n%C2%BA-01-1941"> acessar página do livro para teste de leitura</a> </button>
 
-        <img src="/public/img/1-2.jpg" alt="capitain america">
-      </section>
+        <a href="https://pt.scribd.com/document/596897611/Capitao-America-Comics-n%C2%BA-01-1941">Acessar página do livro</a>
+
+       </div>
+
+       <div id="img">
+        <img src="/public/img/1-2.jpg"  alt="capitao america" >
+      </div>
+
+     </section>
 
 
       </header>
   </body>
 </template>
-<style>
-bottom{
-color: red;
-}
-
-</style>
