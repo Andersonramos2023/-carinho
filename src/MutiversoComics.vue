aa4f67b83305c0b42d06b2b6a74eebc75ac07601
Andersonramos2023 <andersonrsanjo@gmail.com>
2025-04-25 15:13:51 -0300

    carinho.1

--- src/MutiversoComics.vue
+++ src/MutiversoComics.vue
@@ -4,38 +4,78 @@
     <link rel="stylesheet" href="components/MutiversoComics.css">
 
   </head>
-  <body>
-      <header>
 
-        <div class="cabecalho">
-  <a href="MutiversoComics.vue">Multiverso Comics</a>
+  <body>
+    <header>
+
+      <div>
+      <a href="MutiversoComics.vue">Multiverso Comics</a>
+
+        <nav>
+          <ul>
+            <li><a href="#">Termos</a></li>
+            <li> <a href="#">Equipe</a></li>
+            <li><a href="#">Envio</a></li>
+           <li><a href="#">Devolução</a></li>
+          </ul>
+        </nav>
+      </div>
+</header>
+      <section>
+
+        <div>
+
+          <a
+            href="http://pt.wikipedia.org/wiki/Stan_Lee#:~:text=Stanley%20Martin%20Lieber%2C%20mais%20conhecido,empres%C3%A1rio%20e%20ator%20norte-americano.">Altor
+            da comic</a>
+
+
+          <h1>Stanley Martin Lieber</h1>
+
+          <p>
+            Mais conhecido como Stan Lee (Nova Iorque, 28 de dezembro de 1922 — Los Angeles, 12 de novembro de 2018),
+            foi um escritor, editor, publicitário, produtor, diretor, empresário e ator norte-americano, Mais conhecido
+            como o criador da Marvel.
+          </p>
+
+
+          <a href="https://pt.scribd.com/document/596897611/Capitao-America-Comics-n%C2%BA-01-1941">Acessar página do
+            livro</a>
+
         </div>
 
-        <section class="Altor">
+        <img src="/public/images/1-2.jpg" alt="capitan america">
+      </section>
 
-          <div id="tset">
+      <section class="emoji">
 
-            <a href="http://pt.wikipedia.org/wiki/Stan_Lee#:~:text=Stanley%20Martin%20Lieber%2C%20mais%20conhecido,empres%C3%A1rio%20e%20ator%20norte-americano.">Altor da comic</a>
+      </section>
+
+      <section class="comics">
+        <h1>Lançamentos</h1>
+
+        <div>
+          <img src="/public/images/Batman06Rebirth.jpg" alt="batman">
+
+          <h1>DC universe Batman </h1>
+
+        </div>
+
+        <div>
+          <img src="/public/images/images (1).jpeg" alt="invincible">
+
+            <h1>Invincible</h1>
+
+        </div>
+
+        <div>
+          <img src="/public/images/images (2).jpeg" alt="Marvel">
+        </div>
+        <div></div>
 
 
-        <h1>Stanley Martin Lieber</h1>
-
-        <p>
-          Mais conhecido como Stan Lee (Nova Iorque, 28 de dezembro de 1922 — Los Angeles, 12 de novembro de 2018), foi um escritor, editor, publicitário, produtor, diretor, empresário e ator norte-americano, Mais conhecido como o criador da Marvel.
-        </p>
+      </section>
 
 
-        <a href="https://pt.scribd.com/document/596897611/Capitao-America-Comics-n%C2%BA-01-1941">Acessar página do livro</a>
-
-       </div>
-
-       <div id="img">
-        <img src="/public/img/1-2.jpg"  alt="capitao america" >
-      </div>
-
-     </section>
-
-
-      </header>
   </body>
 </template>
